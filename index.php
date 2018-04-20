6cf68604194430d656880dfb02e03e8dd13c4b0d
nenedear <38559761+nenedear@users.noreply.github.com>
2018-04-20 13:42:38 +0100

    Added stage 2 task

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php 
+date_default_timezone_set('Africa/Lagos');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>first task</title>
+</head>
+<style>
+    .task{
+        background-size: cover;
+        width: 100%;
+        
+    }
+    h1{
+        color:white;
+        
+    }
+    .time{
+        padding-left:70%;
+        padding-top:350px;
+    }
+</style>
+<body>
+    <section  class="task" style ="background-image:url(Chrysanthemum.jpg); height:600px" id="section">
+    <!-- <img src="Chrysanthemum.jpg"> -->
+    <div class= "time">
+    <h1><?php echo date("h:i:sa")?>THURSDAY<br><br>APRIL 19<br><br>HNG INTERNSHIP</h1>
+    </div>
+    </section>
+    
+    
+</body>
+</html>
